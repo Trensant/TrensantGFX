cdb21d976460a6748f255db4270f9410b04d656a
Ryeland Gongora <rsgmon@gmail.com>
2017-07-11 10:01:29 -0700

    removed consolelog

--- libs/tgw.js
+++ libs/tgw.js
@@ -1475,7 +1475,7 @@ tgw.d3Chord = function(data, id, options) {
     node.append("text")
       .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
       .selectAll("tspan")
-      .data(function(d) { console.log(d); return d.data.name.split(/(?=[A-Z][^A-Z])/g); })
+      .data(function(d) { return d.data.name.split(/(?=[A-Z][^A-Z])/g); })
       .enter().append("tspan")
       .attr("x", 0)
       .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
