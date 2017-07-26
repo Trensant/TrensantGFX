02c386122357e4da712b71016e7b7dca76728403
Ryeland Gongora <rsgmon@gmail.com>
2017-07-26 14:54:49 -0700

    getting choropleth going

--- libs/tgw.js
+++ libs/tgw.js
@@ -2166,7 +2166,85 @@
   }
 //  =================================================================================
   tgw.d3Choropleth = function (data, div_id, options) {
-    console.log(ca.type)
+    
+	var svg = d3.select("#" + div_id).append("svg"),
+      width = 960; //+svg.attr("width"),
+      height = 600; //+svg.attr("height");
+
+	  svg.attr("width", width).attr("height", height)
+	  
+    var unemployment = d3.map();
+
+    var path = d3.geoPath();
+
+    var x = d3.scaleLinear()
+      .domain([1, 10])
+      .rangeRound([600, 860]);
+
+    var color = d3.scaleThreshold()
+      .domain(d3.range(2, 10))
+      .range(d3.schemeBlues[9]);
+
+    var g = svg.append("g")
+      .attr("class", "key")
+      .attr("transform", "translate(0,40)");
+
+    g.selectAll("rect")
+      .data(color.range().map(function(d) {
+        d = color.invertExtent(d);
+        if (d[0] == null) d[0] = x.domain()[0];
+        if (d[1] == null) d[1] = x.domain()[1];
+        return d;
+      }))
+      .enter().append("rect")
+      .attr("height", 8)
+      .attr("x", function(d) { return x(d[0]); })
+      .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+      .attr("fill", function(d) { return color(d[0]); });
+
+    g.append("text")
+      .attr("class", "caption")
+      .attr("x", x.range()[0])
+      .attr("y", -6)
+      .attr("fill", "#000")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text("Unemployment rate");
+
+    g.call(d3.axisBottom(x)
+      .tickSize(13)
+      .tickFormat(function(x, i) { return i ? x : x + "%"; })
+      .tickValues(color.domain()))
+      .select(".domain")
+      .remove();
+
+    // d3.queue()
+    //   .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+    //   .defer(d3.tsv, "unemployment.tsv", function(d) { unemployment.set(d.id, +d.rate); })
+    //   .await(ready);
+	
+    ready(data)
+	
+    function ready(us) {
+		
+      // if (error) throw error;
+		
+      svg.append("g")
+        .attr("class", "d3Counties")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.tracts).features)
+        .enter().append("path")
+        // .attr("fill", function(d) { console.log(d); return color(d.rate = unemployment.get(d.id)); })
+        // .attr("d", path)
+        // .append("title")
+        // .text(function(d) { return d.rate + "%"; });
+
+      // svg.append("path")
+        // .datum(topojson.mesh(us, us.objects.tracts, function(a, b) { return a !== b; }))
+        // .attr("class", "d3States")
+        // .attr("d", path);
+    }
+
   }
 
 })(typeof tgw === 'undefined' ? this['tgw'] = {} : tgw);//(window.hf = window.hf || {});
