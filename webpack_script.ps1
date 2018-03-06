787d6129000b0d017a8fc73415254229e1387fb0
Ryeland Gongora <rsgmon@gmail.com>
2018-03-05 21:38:53 -0800

    updated syntax on webpack_script.ps1

--- webpack_script.ps1
+++ webpack_script.ps1
@@ -1,5 +1,5 @@
 webpack
-webpack -config .\webpack-dependencies.config.js
+webpack --config .\webpack-dependencies.config.js
 $env:PROD_ENV=1
 webpack
 webpack --config .\webpack-dependencies.config.js
